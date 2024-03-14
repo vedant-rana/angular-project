d8fbeb34887ec32859f96483d440a4f3c00a5844
VR-007 <96051866+VR-007@users.noreply.github.com>
2024-03-14 23:23:22 +0530

    products added

--- e-commerce-app/src/app/components/home/home.component.ts
+++ e-commerce-app/src/app/components/home/home.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../Models/data-types';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  popularProducts: undefined | Product[];
+  trendyProducts: undefined | Product[];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getPopularProducts().subscribe((products) => {
+      this.popularProducts = products;
+    });
+
+    this.productService.getTrendyProducts().subscribe((products) => {
+      this.trendyProducts = products;
+    });
+  }
 }
--- e-commerce-app/src/app/services/product.service.ts
+++ e-commerce-app/src/app/services/product.service.ts
@@ -3,31 +3,41 @@ import { Injectable } from '@angular/core';
 import { Product } from '../Models/data-types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-
-  createProduct(data: Product){
-    return this.http
-      .post('http://localhost:3000/products', data, { observe: 'response' })
+  createProduct(data: Product) {
+    return this.http.post('http://localhost:3000/products', data, {
+      observe: 'response',
+    });
   }
 
-  getAllProducts(){
+  getAllProducts() {
     return this.http.get<Product[]>('http://localhost:3000/products');
   }
 
-  deleteProductById(id: string | undefined){
+  deleteProductById(id: string | undefined) {
     return this.http.delete<Product>(`http://localhost:3000/products/${id}`);
   }
 
-  getProductById(id:string){
-    return this.http.get<Product>(`http://localhost:3000/products/${id}`)
+  getProductById(id: string) {
+    return this.http.get<Product>(`http://localhost:3000/products/${id}`);
   }
 
-  updateProduct(data : Product){
-    return this.http.put<Product>(`http://localhost:3000/products/${data.id}`, data);
+  updateProduct(data: Product) {
+    return this.http.put<Product>(
+      `http://localhost:3000/products/${data.id}`,
+      data
+    );
   }
 
+  getPopularProducts() {
+    return this.http.get<Product[]>(`http://localhost:3000/products?_limit=3`);
+  }
+
+  getTrendyProducts() {
+    return this.http.get<Product[]>(`http://localhost:3000/products?_limit=8`);
+  }
 }
